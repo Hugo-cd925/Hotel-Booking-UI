3feaf5bf5458f80c02bf44f0be09a5ddf0e82115
Hugo Delgado <hugo.cdelgado925@gmail.com>
2022-09-02 15:44:57 -0700

    add CRUD routes for rooms and reservations

---
+++ src/components/create-reservations/CreateReservations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateReservations = () => {
+  return (
+    <div>CreateReservations</div>
+  )
+}
+
+export default CreateReservations;
---
+++ src/components/edit-reservations/EditReservations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditReservations = () => {
+  return (
+    <div>EditReservations</div>
+  )
+}
+
+export default EditReservations
